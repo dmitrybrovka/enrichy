00106d15fded279078a4b0ca5bfb75cf1c18fe5f
dmitrybrovka <dmitrybrovka@yandex-team.ru>
2023-04-24 17:17:04 -0300

    WIP: finalizing work

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,19 +1,26 @@
 'use strict';
 
+/**
+ * {@link https://github.com/sparksuite/simplemde-markdown-editor}
+ */
+const areaConfig = {
+	element: area,
+	spellChecker: false,
+	status: false,
+	autoDownloadFontAwesome: false,
+	forceSync: true
+}
+
 function richyAreas(dom = document) {
 	const areas = dom.getElementsByTagName != null ? Array.from(dom.getElementsByTagName('textarea')) : [];
 
 	areas.forEach((area) => {
 		if (!area.closest('.CodeMirror')) {
-			new SimpleMDE({element: area, autoDownloadFontAwesome: false, forceSync: true});
+			const mde = new SimpleMDE(areaConfig);
+			mde.codemirror.on('change', () => area.dispatchEvent(new Event('input', {bubbles: true})));
 		}
 	});
 }
-
-const observerConfig = {
-	childList: true,
-	subtree: true
-};
 
 function debounce(fn, ms = 500) {
 	let t;
@@ -25,6 +32,11 @@ function debounce(fn, ms = 500) {
 }
 
 function startDaemon() {
+	const observerConfig = {
+		childList: true,
+		subtree: true
+	};
+
 	const observer = new MutationObserver(debounce((mutations) => {
 		mutations.forEach((mut) => {
 			if ([...mut.addedNodes].length > 0) {
